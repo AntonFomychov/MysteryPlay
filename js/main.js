d0a168a95c91907f12978720bae88dffd1cbbb5e
AntonFomychov <anton.a.fomichov@gmail.com>
2026-06-08 17:21:54 +0200

    animate section - desktop

--- js/main.js
+++ js/main.js
@@ -478,4 +478,15 @@ $(document).ready(function () {
 		$parent.toggleClass("open");
 		$content.stop(true, true).slideToggle(300);
 	});
+
+	const observer = new IntersectionObserver((entries) => {
+	  	entries.forEach(entry => {
+	    	if (entry.isIntersecting) {
+	      		entry.target.classList.add('animate');
+	      		observer.unobserve(entry.target);
+	    	}
+	  	});
+	}, { threshold: 0.2 });
+
+	document.querySelectorAll('.animate-blocks').forEach(el => observer.observe(el));
 });
